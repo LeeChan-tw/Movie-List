84850b3d30c84947a6347f3ec5a8ad3874b9a1e7
LeeChan-tw <65239235+LeeChan-tw@users.noreply.github.com>
2020-08-24 23:58:01 +0800

    IndexAPI_renderData

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-const BASE_URL = "https://movie-list.alphacamp.io";
-const INDEX_URL = BASE_URL + "/api/v1/movies/";
-const SHOW_URL = BASE_URL + "/api/v1/movies/1/";
-const POSTER_URL = BASE_URL + "/poster/";
+const BASE_URL = 'https://movie-list.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
+const POSTER_URL = BASE_URL + '/posters/'
 
-const movies = [];
+const movies = []
+
+const dataPanel = document.querySelector('#data-panel')
+
+function renderMovieList(data) {
+    let rawHTML = ''
+
+    data.forEach((item) => {
+        //title, image
+        console.log(item)
+        rawHTML += `
+        <div class="col-sm-3">
+        <div class="mb-2">
+            <div class="card">
+              <img
+                src="${POSTER_URL + item.image}"
+                class="card-img-top"
+                alt = "Movie Poster Of ${item.title}"
+              />
+              <div class="card-body">
+                <h5 class="card-title">${item.title}</h5>
+              </div>
+              <div class="card-footer">
+                <button
+                  class="btn btn-primary btn-show-movie"
+                  data-toggle="modal"
+                  data-target="#movie-modal"
+                >
+                  More
+                </button>
+                <button class="btn btn-info btn-add-favorite">+</button>
+              </div>
+            </div>
+          </div>
+          </div>                
+          `
+    })
+
+    dataPanel.innerHTML = rawHTML
+}
 
 axios.get(INDEX_URL).then((response) => {
-  movies.push(...response.data.results);
-  console.log(movies);
-});
+    movies.push(...response.data.results)
+    renderMovieList(movies)
+})
