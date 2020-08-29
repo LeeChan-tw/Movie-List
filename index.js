87a4f689254ca2bd2294893f6531e1526090845c
LeeChan <lee79102@hotmail.com>
2020-08-30 00:22:43 +0800

    SearchBar

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const POSTER_URL = BASE_URL + '/posters/'
 const movies = []
 
 const dataPanel = document.querySelector('#data-panel')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#search-input')
 
 function renderMovieList(data) {
     let rawHTML = ''
@@ -53,6 +55,30 @@ dataPanel.addEventListener('click', (event) => {
     if (event.target.matches('.btn-show-movie')) {
         showMovieModal(event.target.dataset.id)
     }
+})
+
+searchForm.addEventListener('submit', function onSearchFormSubmit(event) {
+    event.preventDefault()
+    const keyword = searchInput.value.trim().toLowerCase()
+    let filteredMovies = []
+
+    //if (!keyword.length) {
+    //    return alert('Please enter a valid string')
+    // }
+
+    filteredMovies = movies.filter((movie) => movie.title.toLowerCase().includes(keyword))
+
+    if (filteredMovies.length === 0) {
+        return alert('Cannot find movies with keyword: ' + keyword)
+    }
+
+    // for (const movie of movies) {
+    //     if (movie.title.toLowerCase().includes(keyword)) {
+    //         filteredMovies.push(movie)
+    //     }
+    // }
+
+    renderMovieList(filteredMovies)
 })
 
 function showMovieModal(id) {
